d40a648856ed50c2344705079a88d1323ef53bf6
Gouse.C <140190450+Gouse-c@users.noreply.github.com>
2024-06-25 18:46:16 +0530

    Add files via upload

---
+++ ConstructorExample.java
@@ -0,0 +1,11 @@
+class ConstructorExample
+{
+	public ConstructorExample()
+	{
+		System.out.println("Hello World");
+	}
+	public static void main(String args[])
+	{
+		ConstructorExample c1 = new ConstructorExample();
+	}
+}
